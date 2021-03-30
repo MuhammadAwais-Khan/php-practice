afdf5dc817dadf4af2be63fc7dd0dc7b4c88c61e
MuhammadAwais-Khan <awaiskhan.se@gmail.com>
2021-03-30 13:53:15 +0500

    strlen

---
+++ strlen.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "<i>strlen function Start</i><br>";
+
+echo strlen("Hello world!");
+
+echo "<br><br>";
+echo strlen("Media Park");
+
+echo "<br><br>";
+echo strlen("hello how are you");
+
+echo "<br><br><i>strlen function End</i>";
