e4f3935ff573bcb6a191104a559e9e9859fcc1d8
MuhammadAwais-Khan <awaiskhan.se@gmail.com>
2021-03-30 11:43:07 +0500

    pack-unpack

--- pack-unpack.php
+++ pack-unpack.php
@@ -4,17 +4,40 @@ echo "<h3>Pack</h3>";
 
 echo pack("C3",80,72,80);
 
-echo "<br>";
-echo "<br>";
+echo "<br><br>";
 
 echo pack("A4", 71898);
 
-echo "<br>";
-echo "<br>";
+echo "<br><br>";
 
 echo pack("a5", "media park");
 
-echo "<br>";
-echo "<br>";
+echo "<br><br>";
 
 echo pack("a10", "media park");
+
+echo "<br><br>";
+
+echo "-------------------------";
+
+echo "<h3>UnPack</h3>";
+
+echo "<br><br>";
+
+$binary_data = pack("i3", 56, 439, 54);
+var_dump(unpack("i3", $binary_data));
+
+echo "<br><br>";
+
+$data = "PHP";
+print_r(unpack("C*",$data));
+
+
+echo "<br><br>";
+
+$data = "Media Park";
+print_r(unpack("s*int ",$data));
+echo "<br><br>";
+print_r(unpack("H*int ",$data));
+echo "<br><br>";
+print_r(unpack("h*int ",$data));
