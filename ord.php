7c43db91f128827d9bb30d4ac2d8f359bd53aa6e
MuhammadAwais-Khan <awaiskhan.se@gmail.com>
2021-03-30 12:53:41 +0500

    ord

---
+++ ord.php
@@ -0,0 +1,13 @@
+<?php
+echo "<i>Ord function Start</i><br>";
+echo "<br>";
+echo ord('great');
+
+echo "<br><br>";
+
+echo ord('media');
+echo "<br><br>";
+
+echo ord('park');
+
+echo "<br><br><i>Ord function End</i>";
