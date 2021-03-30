013c643169dae90c48af23cd875e0a9ce08110ae
MuhammadAwais-Khan <awaiskhan.se@gmail.com>
2021-03-30 11:34:28 +0500

    pack-unpack

---
+++ pack-unpack.php
@@ -0,0 +1,20 @@
+<?php
+
+echo "<h3>Pack</h3>";
+
+echo pack("C3",80,72,80);
+
+echo "<br>";
+echo "<br>";
+
+echo pack("A4", 71898);
+
+echo "<br>";
+echo "<br>";
+
+echo pack("a5", "media park");
+
+echo "<br>";
+echo "<br>";
+
+echo pack("a10", "media park");
